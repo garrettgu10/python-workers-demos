0ec0d8a54a9fac8b4d7b7dfe4d61c9a20ffe5b3c
Brendan Irvine-Broque <brendanib@gmail.com>
2024-03-06 13:27:01 -0800

    fetch to on_fetch for langchain example

--- langchain/worker.py
+++ langchain/worker.py
@@ -1,7 +1,7 @@
 from js import Response
 from langchain.chat_models import ChatOpenAI
 
-def fetch(request, env):
+def on_fetch(request, env):
     ChatOpenAI(openai_api_key=env.API_KEY)
     print("Constructed OpenAI object")
 
