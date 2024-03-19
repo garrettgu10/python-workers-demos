9ed0036d5d61f8155a24ff50448a28c3a7e0a8b5
Garrett Gu <garrett@cloudflare.com>
2024-03-19 01:30:58 +0000

    fix langchain demo

--- langchain/worker.py
+++ langchain/worker.py
@@ -2,12 +2,10 @@ from js import Response
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import OpenAI
 
-API_KEY = "sk-abcdefg"
-
-async def test(request):
+async def on_fetch(request, env):
   prompt = PromptTemplate.from_template("Complete the following sentence: I am a {profession} and ")
-  llm = OpenAI(api_key=API_KEY)
+  llm = OpenAI(api_key=env.API_KEY)
   chain = prompt | llm
 
   res = await chain.ainvoke({"profession": "electrician"})
-  print(res)
+  return Response.new(res.split(".")[0].strip())
